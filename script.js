4bd5c9aba7d74513207c698d61033a581a493f5a
dkphl <dkae0809@gmail.com>
2018-08-18 22:59:07 -0400

    tentative solution, delete / mark done functionality

--- script.js
+++ script.js
@@ -8,44 +8,44 @@ function checkInputLength() {
 }
 
 function addDoneButton () { 
-	var doneButton = document.createElement('button');
-	doneButton.textContent = "Mark Done";
-	doneButton.className = "doneButton";
-	doneButton.addEventListener("click", function() {
-		doneButton.parentElement.classList.toggle('done');
-	});
-	return doneButton;
+  var doneButton = document.createElement('button');
+  doneButton.textContent = "Mark Done";
+  doneButton.className = "doneButton";
+  doneButton.addEventListener("click", function() {
+    doneButton.parentElement.classList.toggle('done');
+  });
+  return doneButton;
 }
 
 function addDeleteButton () { 
-	var deleteButton = document.createElement('button');
-	deleteButton.textContent = "Delete";
-	deleteButton.className = "deleteButton";
-	deleteButton.addEventListener("click", function() {
-		deleteButton.parentElement.parentElement.removeChild(deleteButton.parentElement);
-	});
-	return deleteButton;
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "deleteButton";
+  deleteButton.addEventListener("click", function() {
+    deleteButton.parentElement.parentElement.removeChild(deleteButton.parentElement);
+  });
+  return deleteButton;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	li.appendChild(addDoneButton());
-	li.appendChild(addDeleteButton());
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  li.appendChild(addDoneButton());
+  li.appendChild(addDeleteButton());
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick () {
-	if (checkInputLength() > 0) {
-		createListElement();
-	}
+  if (checkInputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (checkInputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (checkInputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
 button.addEventListener("click", addListAfterClick);
